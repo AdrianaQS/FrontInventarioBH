a66108a3da7c538a8e7b04418acafbcf8ffa9811
Merly <merlyvallejos22@gmail.com>
2022-05-27 01:23:04 -0500

    Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './components/notfound/notfound.component';
-
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 export const APP_ROUTES: Routes = [
   {
     path: 'login',
     loadChildren: () =>
       import('./components/login/login.module').then(mod => mod.LoginModule)
   },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  
   {
     path: '**',
     component: NotfoundComponent

bd894c634fd31bba1a6609b0978aab617594854b
balocenm <balocenm@gmail.com>
2022-07-27 19:51:53 -0500

    reporte pdf

--- src/app/components/pedidos/pedidos.component.ts
+++ src/app/components/pedidos/pedidos.component.ts
@@ -4,6 +4,8 @@ import { PedidoService } from 'src/app/services/pedido.service';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { InsumoService } from 'src/app/services/insumo.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-pedidos',
@@ -254,4 +256,32 @@ export class PedidosComponent implements OnInit {
       this.getPedidos();
     });
   }
+
+  downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('htmlData') !== undefined ? document.getElementById('htmlData') : null;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    if (DATA !== null) {
+      html2canvas(DATA, options).then((canvas) => {
+
+        const img = canvas.toDataURL('image/PNG');
+
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        return doc;
+      }).then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      });
+    }
+  }
+
 }
--- src/app/components/salidas/salidas.component.ts
+++ src/app/components/salidas/salidas.component.ts
@@ -66,28 +66,45 @@ export class SalidasComponent implements OnInit {
     this.getListaInsumos();
   }
 
-  public downloadPDF(): void {
-    const doc = new jsPDF();
+  downloadPDF() {
+    // Extraemos el
+    const DATA = document.getElementById('htmlData') !== undefined ? document.getElementById('htmlData') : null;
+    const doc = new jsPDF('p', 'pt', 'a4');
+    const options = {
+      background: 'white',
+      scale: 3
+    };
+    if (DATA !== null) {
+      html2canvas(DATA, options).then((canvas) => {
 
-    doc.text('Hello world!', 10, 10);
-    doc.save('hello-world.pdf');
+        const img = canvas.toDataURL('image/PNG');
+
+        // Add image Canvas to PDF
+        const bufferX = 15;
+        const bufferY = 15;
+        const imgProps = (doc as any).getImageProperties(img);
+        const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
+        return doc;
+      }).then((docResult) => {
+        docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
+      });
+    }
   }
+
 
   getSalidas() {
     this.salidaService.getSalidas().subscribe((res: any) => {
       this.arraySalidas = res;
-      console.log(this.arraySalidas, 'lista getSalidas');
     });
   }
 
   getListaInsumos() {
     this.insumoService.getInsumos().subscribe((res: any) => {
       this.arrayInsumos = res;
-      console.log(this.arrayInsumos, 'lista insumos');
     });
   }
-
-
 
   get getInsumos() {
     return this.salidaForm.get('insumos') as FormArray;
